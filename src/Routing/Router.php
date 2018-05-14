b1d217f74b6f7d9bf373aafadb160eb5fdbd18a4
yanghaonan <yanghaonan@hupu.com>
2018-05-14 11:58:07 +0800

    router增加getCurrentRoute获取当前路由信息

--- src/Routing/Router.php
+++ src/Routing/Router.php
@@ -472,4 +472,14 @@ class Router
 	{
 		return isset($this->currentRoute['1']) ? $this->currentRoute['1']: [];
 	}
+
+    /**
+     * 获取当前路由信息
+     *
+     * @return array
+     */
+    public function getCurrentRoute()
+    {
+        return isset($this->currentRoute) ? $this->currentRoute: [];
+    }
 }
